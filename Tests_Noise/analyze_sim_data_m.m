96ce929e354a52136ce27aa4f2927177dcf01cdd
a-gradim <andregradim@gmail.com>
2018-01-08 17:36:30 +0000

    Added auto export of images, surf plots changed to contour3, conversion of m to HPA

--- Tests_Noise/analyze_sim_data_m.m
+++ Tests_Noise/analyze_sim_data_m.m
@@ -12,11 +12,15 @@ close all;
 addpath('../../ProjGeom');
 addpath('../');
 
-
+%%
+%Path for figure export
+path_fig = './figures/';
+%String to be used to concat name of figure
+tempstr={'max_'; 'mean_'; 'std_'};
 
 %% simulation parameters
 %emitter
-params.n_Emitters = 4; %square mode
+params.n_Emitters = 4; %square mode 4/9/16
 params.m =1;
 params.Pb = 1;
 params.Ps = 0.25;
@@ -29,18 +33,25 @@ params.L = 7.5;%not to be changed
 params.H = 2.5;%not to be changed
 
 %receiver
-params.Np = 28;
-params.Nm = 70;
+%28:32
+params.Np = 31; %28/29
+%70:74
+params.Nm = 70; %70/71/72
 params.Psi = 20;
 params.SR = 0.25;
 
 %simulation resolution
 Wstep = 0.5;
 
+% add number of emitters to differentiate the graphs
+path_fig= [path_fig 'n_em_' num2str(params.n_Emitters) '/'];
+
 %% Parameter to be changed in the simulation
 % m1 = [1,2,3,4,5,6];
 % Psi1 = [5 7.5 10 12.5];
 M1 = [1 2 3 4 5 6];
+
+HPA=[60 45 37.46 32.75  29.45 27];
 Psi1 = [5 7.5 10 12.5];
 
 %from the comment above choose a value to set m and Psi
@@ -64,8 +75,14 @@ for iPsi = 1:numel(Psi1)
         for i = [9 10 11 2]
             filename = [ filename num2str(getfield(params,field{i})) '-'] ;
         end
-        load(['./res/square_' filename num2str(Wstep) '.mat'], 'res',...
+        %if(params.n_Emitters == 4)
+           % load(['./res/square_' filename num2str(Wstep) '.mat'], 'res',...
+           % 'params','underPercentage','broken');
+        %else
+             load(['./res/square_' filename num2str(Wstep) '-' num2str(getfield(params,field{1})) '.mat'], 'res',...
             'params','underPercentage','broken');
+        
+        %end
         
         %place data from file in struct for easier access
         data(index).res=res;
@@ -95,7 +112,7 @@ for index = 1: numel(data)
 end
 
 %% plot max,mean and std vs m
-for i = 1:numel(M1)
+for i = 1:numel(HPA)
     figure(1)
     plot(Psi1,mMax(i,:),'*-')
     hold on
@@ -118,43 +135,108 @@ title({'Standard Deviation Error vs Psi';...
 
 for i = 1:3
     figure(i)
-    
     set(gca,'XTick',Psi1)
     legend('m=1','m=2', 'm=3', 'm=4', 'm=5', 'm=6','Location','SouthWest')
     ylabel('Error(meters)')
     xlabel('Psi')
+    saveas(gcf, [path_fig 'psi_to_' tempstr{i}  num2str(params.Np) '_np_' num2str(params.Nm) '_nm.png'], 'png')
 end
 
 %% plot max,mean,std vs m
 for i = 1:numel(Psi1)
     figure(4)
-    plot(M1,mMax(:,i),'-*')
+    plot(HPA,mMax(:,i),'-*')
     hold on
     figure(5)
-    plot(M1,mMean(:,i),'-*')
+    plot(HPA,mMean(:,i),'-*')
     hold on
     figure(6)
-    plot(M1,mStd(:,i),'-*')
+    plot(HPA,mStd(:,i),'-*')
     hold on
 end
 figure(4)
-title({'Max Error vs Lambertian Index';...
+title({'Max Error vs HPA';...
     [num2str(params.Np) ' Parallels, ' num2str(params.Nm) ' Meridians']})
 figure(5)
-title({'Mean Error vs Lambertian Index';...
+title({'Mean Error vs HPA';...
     [num2str(params.Np) ' Parallels, ' num2str(params.Nm) ' Meridians']})
 figure(6)
-title({'Standard Deviation Error vs Lambertian Index';...
+title({'Standard Deviation Error vs HPA';...
     [num2str(params.Np) ' Parallels, ' num2str(params.Nm) ' Meridians']})
 
 for i = 4:6
     figure(i)
     
-    set(gca,'XTick',(M1(1):1:M1(end)))
+    %     set(gca,'XTick',(HPA))
     legend('Psi = 5','Psi = 7.5', 'Psi = 10', 'Psi = 12.5')
     ylabel('Error(meters)')
-    xlabel('m - Lambertian Index')
+    xlabel('HPA')
+    saveas(gcf, [path_fig 'hpa_to_' tempstr{i-3}  num2str(params.Np) '_np_' num2str(params.Nm) '_nm.png'], 'png')
 end
+
+%% 
+
+figure
+contour3(Psi1,HPA,mMax,20)
+xlabel('Psi')
+ylabel('HPA')
+shading interp
+view(2)
+colorbar
+
+title({'Max Error vs HPA';...
+    [num2str(params.Np) ' Parallels, ' num2str(params.Nm) ' Meridians']})
+saveas(gcf, [path_fig 'psi_hpa_to_max_' num2str(params.Np) '_np_' num2str(params.Nm) '_nm.png'], 'png')
+
+figure
+contour3(Psi1,HPA,mMean,20)
+xlabel('Psi')
+ylabel('HPA')
+shading interp
+view(2)
+colorbar
+
+title({'Mean Error vs HPA';...
+    [num2str(params.Np) ' Parallels, ' num2str(params.Nm) ' Meridians']})
+saveas(gcf, [path_fig 'psi_hpa_to_mean_' num2str(params.Np) '_np_' num2str(params.Nm) '_nm.png'], 'png')
+
+figure
+contour3(Psi1,HPA,mStd,20)
+xlabel('Psi')
+ylabel('HPA')
+shading interp
+view(2)
+colorbar
+
+title({'Standard Deviation Error vs HPA';...
+    [num2str(params.Np) ' Parallels, ' num2str(params.Nm) ' Meridians']})
+
+saveas(gcf, [path_fig 'psi_hpa_to_std_' num2str(params.Np) '_np_' num2str(params.Nm) '_nm.png'], 'png')
+
+%%
+% % % fig = get(0, 'children');
+% % % 
+% % % % Alterar as propriedades das figuras. Resize para a resolução do ecrã e
+% % % % indicar que a imagem é para ser imprimida com o mesmo número de pixeis
+% % % % que possui no ecrã 
+% % % % screensize = get(0, 'ScreenSize');
+% % % % set(fig, 'Position',[0 0 screensize(3) screensize(4)], 'PaperPositionMode','auto');
+% % % 
+% % % % para cada imagem, gravar em formato jpeg
+% % % for k = 1 : length(fig)
+% % %     saveas(fig(k), [fig(k).CurrentAxes.Title.String(1) fig(k).CurrentAxes.Title.String(2) '.png'], 'png')
+% % % end;
+
+
+% % % %% Plot histogram error
+% % % min(min(mMean))
+% % % dToPlot= find(mMean == min(min(mMean)));
+% % % figure
+% % % 
+% % % hist([data(dToPlot).res.dist], 100 )
+% % % 
+% % % 
+% % % 
 
 
 
