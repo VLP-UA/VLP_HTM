ef478d6620273898805871bc271f7b530d46ab01
André Gradim <andregradim@gmail.com>
2018-02-22 00:50:33 +0000

    Added trilateration calculation function for the given arguments

---
+++ Tests_Noise/trilateration_group_em.m
@@ -0,0 +1,22 @@
+function [ location ] = trilateration_group_em( Emitters, group, radii )
+%TRILATERATION_GROUP_EM Calculate the position for the group of index
+%indicated by group
+
+
+% Get the emitters position
+temp = [Emitters.HTM];
+pos_Em = temp(1:2,4:4:end);
+%Consider only accepted positions
+pos_Em=pos_Em(:,group);
+% pos_Em = posEm(:,accepted);
+
+Xx = pos_Em(1,:);
+Yy = pos_Em(2,:);
+
+A=[ Xx(2:end)'-Xx(1) Yy(2:end)'-Yy(1)];
+B=0.5*((radii(1)^2-radii(2:end)'.^2) + (Xx(2:end)'.^2+Yy(2:end)'.^2) - (Xx(1)^2 + Yy(1)^2));
+
+location = (A'*A)^(-1)*A'*B;
+
+end
+
